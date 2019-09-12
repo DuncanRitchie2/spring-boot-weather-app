565e458212aa460b9b2919c92b567722b4976053
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-12 15:10:54 +0100

    First commit

---
+++ src/main/java/com/duncanritchie/FirstSpringApp/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.duncanritchie.FirstSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/duncanritchie/FirstSpringApp/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.duncanritchie.FirstSpringApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello world!";
+    }
+}
