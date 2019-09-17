54263b82263a561bb82cf8e62039b236315a4bbc
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-17 14:04:12 +0100

    Commented out sendPOST() because we're not posting anything

--- src/main/java/com/duncanritchie/SpringBootWeatherApp/CodeFromMark.java
+++ src/main/java/com/duncanritchie/SpringBootWeatherApp/CodeFromMark.java
@@ -15,8 +15,8 @@ public class CodeFromMark {
         public static void main(String[] args) throws IOException {
             sendGET();
             System.out.println("GET DONE");
-            sendPOST();
-            System.out.println("POST DONE");
+//            sendPOST();
+//            System.out.println("POST DONE");
         }
         private static void sendGET() throws IOException {
             URL obj = new URL(GET_URL);
@@ -40,48 +40,48 @@ public class CodeFromMark {
                 System.out.println("GET request not worked");
             }
         }
-        private static void sendPOST() throws IOException {
-            URL obj = new URL(POST_URL);
-            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-            con.setRequestMethod("POST");
-            con.setRequestProperty("User-Agent", USER_AGENT);
-            // For POST only - START
-            con.setDoOutput(true);
-            OutputStream os = con.getOutputStream();
-            os.write(POST_PARAMS.getBytes());
-            os.flush();
-            os.close();
-            // For POST only - END
-            int responseCode = con.getResponseCode();
-            System.out.println("POST Response Code :: " + responseCode);
-            if (responseCode == HttpURLConnection.HTTP_OK) { //success
-                BufferedReader in = new BufferedReader(new InputStreamReader(
-                        con.getInputStream()));
-                String inputLine;
-                StringBuffer response = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    response.append(inputLine);
-                }
-                in.close();
-                // print result
-                System.out.println(response.toString());
-                try {
-                    // parsing
-                    Object jsonObj = new JSONParser().parse(response);
-//                    System.out.println(jsonObj);
-
-//                    // typecasting obj to JSONObject
-//                    JSONObject jo = (JSONObject) obj;
-//
-//                    Object currently = jo.get("currently");
-//                    String summary = jo.get("summary").toString();
-//
-//                    System.out.println(summary);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                } finally {}
-            } else {
-                System.out.println("POST request not worked");
-            }
-        }
+//        private static void sendPOST() throws IOException {
+//            URL obj = new URL(POST_URL);
+//            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+//            con.setRequestMethod("POST");
+//            con.setRequestProperty("User-Agent", USER_AGENT);
+//            // For POST only - START
+//            con.setDoOutput(true);
+//            OutputStream os = con.getOutputStream();
+//            os.write(POST_PARAMS.getBytes());
+//            os.flush();
+//            os.close();
+//            // For POST only - END
+//            int responseCode = con.getResponseCode();
+//            System.out.println("POST Response Code :: " + responseCode);
+//            if (responseCode == HttpURLConnection.HTTP_OK) { //success
+//                BufferedReader in = new BufferedReader(new InputStreamReader(
+//                        con.getInputStream()));
+//                String inputLine;
+//                StringBuffer response = new StringBuffer();
+//                while ((inputLine = in.readLine()) != null) {
+//                    response.append(inputLine);
+//                }
+//                in.close();
+//                // print result
+//                System.out.println(response.toString());
+////                try {
+////                    // parsing
+//////                    Object jsonObj = new JSONParser().parse(response);
+//////                    System.out.println(jsonObj);
+////
+//////                    // typecasting obj to JSONObject
+//////                    JSONObject jo = (JSONObject) obj;
+//////
+//////                    Object currently = jo.get("currently");
+//////                    String summary = jo.get("summary").toString();
+//////
+//////                    System.out.println(summary);
+////                } catch (ParseException e) {
+////                    e.printStackTrace();
+////                } finally {}
+//            } else {
+//                System.out.println("POST request not worked");
+//            }
+//        }
 }
