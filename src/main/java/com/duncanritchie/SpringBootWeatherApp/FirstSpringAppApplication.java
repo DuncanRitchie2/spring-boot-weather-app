ed5b6cedf43701a5aec7491a4cca177e1bf5636a
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-24 17:25:08 +0100

    Removed some unused imports

--- src/main/java/com/duncanritchie/SpringBootWeatherApp/FirstSpringAppApplication.java
+++ src/main/java/com/duncanritchie/SpringBootWeatherApp/FirstSpringAppApplication.java
@@ -1,10 +1,7 @@
 package com.duncanritchie.SpringBootWeatherApp;
 
-import com.google.gson.JsonElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class FirstSpringAppApplication {
