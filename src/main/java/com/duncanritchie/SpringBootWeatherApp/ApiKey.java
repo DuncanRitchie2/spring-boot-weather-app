db3e249a1c8bedc731cc10f7dbc6fe992f53100c
Duncan Ritchie <duncanritchie@btopenworld.com>
2019-09-24 17:36:04 +0100

    Inverted try/catch in ApiKey

--- src/main/java/com/duncanritchie/SpringBootWeatherApp/ApiKey.java
+++ src/main/java/com/duncanritchie/SpringBootWeatherApp/ApiKey.java
@@ -4,19 +4,19 @@ public class ApiKey {
 
     public static String getDarkSkyKey() {
         try {
-            return Secrets.getDarkSkyKey();
+            return System.getenv("APIKEY_DARKSKY");
         }
         catch (Exception e) {
-            return System.getenv("APIKEY_DARKSKY");
+            return Secrets.getDarkSkyKey();
         }
     }
 
     public static String getMapboxKey() {
         try {
-            return Secrets.getMapboxKey();
+            return System.getenv("APIKEY_MAPBOX");
         }
         catch (Exception e) {
-            return System.getenv("APIKEY_MAPBOX");
+            return Secrets.getMapboxKey();
         }
     }
 }
